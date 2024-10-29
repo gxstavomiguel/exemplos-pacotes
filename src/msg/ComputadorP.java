992635f6135323239667fe74e81c770c2ed36880
gxstavomiguel <gustavomiguelsilvafc@gmail.com>
2024-10-29 18:34:02 -0300

    Código refatorado com herança

--- src/msg/ComputadorP.java
+++ src/msg/ComputadorP.java
@@ -3,11 +3,21 @@ package msg;
 public class ComputadorP {
     public static void main(String[] args) {
 
+        System.out.println("MSN");
         MSNMessenger msn = new MSNMessenger();
-
         msn.enviarMensagem();
-
         msn.receberMensagem();
+
+        System.out.println("FACEBOOK");
+        FacebookMessenger fcb = new FacebookMessenger();
+        fcb.enviarMensagem();
+        fcb.receberMensagem();
+
+        System.out.println("TELEGRAM");
+        Telegram tlg = new Telegram();
+        tlg.enviarMensagem();
+        tlg.receberMensagem();
+
     }
 
 }
---
+++ src/msg/FacebookMessenger.java
@@ -0,0 +1,5 @@
+package msg;
+
+public class FacebookMessenger extends ServicoMensagemInstantanea {
+
+}
