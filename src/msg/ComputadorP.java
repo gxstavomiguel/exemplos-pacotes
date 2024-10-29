78dcc7107c17bd1d96b518d08cc98b09dbf9c567
gxstavomiguel <gustavomiguelsilvafc@gmail.com>
2024-10-29 18:18:59 -0300

    encapsulamento

---
+++ src/msg/ComputadorP.java
@@ -0,0 +1,13 @@
+package msg;
+
+public class ComputadorP {
+    public static void main(String[] args) {
+
+        MSNMessenger msn = new MSNMessenger();
+
+        msn.enviarMensagem();
+
+        msn.receberMensagem();
+    }
+
+}
