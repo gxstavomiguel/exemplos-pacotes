a00846a2526efa8aaadbee290615572158a82db4
gxstavomiguel <gustavomiguelsilvafc@gmail.com>
2024-10-28 23:08:09 -0300

    teste código2

--- src/escola/Aluno.java
+++ src/escola/Aluno.java
@@ -5,19 +5,30 @@ public class Aluno {
     private String nome;
     private int idade;
 
-    public String getNome(){
+    private String sexo;
+
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getNome() {
         return nome;
     }
 
-    public void setNome(String newNome){
-        nome = newNome;
+    public void setNome(String nome) {
+        this.nome = nome;
     }
 
-    public int getIdade(){
+    public int getIdade() {
         return idade;
     }
 
-    public void setIdade(int newIdade){
-        this.idade = newIdade;
+    public void setIdade(int idade) {
+        this.idade = idade;
     }
 }
